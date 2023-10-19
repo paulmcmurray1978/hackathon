51b2b1e7d31d5deb396bcca5a70a59ad77dd20d8
paulmcmurray-digital <paul.mcmurray@digital.mod.uk>
2023-10-19 13:01:10 +0100

    basic routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from "./pages/Layout";
+import Home from "./components/Home/Home";
 import About from "./components/About/About";
-function App() {
+// import Blogs from "./pages/Blogs";
+// import Contact from "./pages/Contact";
+// import NoPage from "./pages/NoPage";
+
+export default function App() {
   return (
-    <div className="App">
-      <div style={{ backgroundImage: `url(${background})` }}>Hello World</div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
